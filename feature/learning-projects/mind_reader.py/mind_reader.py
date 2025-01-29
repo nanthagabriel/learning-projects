e2ab6dfa665f314f272b719653f2f5fe3e27ca6a
Nantha Gabriel <nanthagabriel@gmail.com>
2025-01-30 05:55:38 +0800

    Update mind_reader.py

--- feature/learning-projects/mind_reader.py/mind_reader.py
+++ feature/learning-projects/mind_reader.py/mind_reader.py
@@ -9,34 +9,34 @@ secret_number = random.randint (1, 99)
 # Greeting
 print("I'm thinking of a number between 1 and 99.")
 
-# The maximum number of attempts given.
+# The maximum number of attempts given to the user.
 num_of_attempts = 1
 max_num_of_attempts = 10
 
 # User input of guesses. 
 guess = int(input(f"Can you guess what number it is? .. I'll give you {max_num_of_attempts} attempts. Go : ")) 
 
-# Loop till '10' attempt are over. 
+# Loop till '10' attempts are over. 
 while guess != secret_number and num_of_attempts < max_num_of_attempts:
     
     # Clues are given to narrow down the user's guesses. 
     if guess < secret_number:
-        print("uh uh aah .. that's too low")
+        print("no no no .. that's too low")
     else :
-        print("So close! .. but thats too high")
+        print(" aye yai yai  .. thats too high")
 
     # Plus '1' every time the user fails an attempt.
     num_of_attempts += 1
 
-    # Countdown of the number of attempts the user has and user input for another attempt.
+    # Reveal of the number of attempts left and receive user input for another guess.
     if num_of_attempts < max_num_of_attempts:
-         guess = int(input(f"\nDon't give up! .. You have {num_of_attempts} / {max_num_of_attempts} left .. C'mon now make another guess! : "))
+         guess = int(input(f"\nDon't give up! .. We're at {num_of_attempts} / {max_num_of_attempts} attempts .. C'mon now make another guess! : "))
     
     # After '10' failed attempts, the secret number is revealed.
     else:
          print(f"Oh well, i guess you're not a mind reader, the number i was thinking of was {secret_number}. ")
          break
 
-# Congratulate the user and reveal how attempts was taken to narrow down to the answer. 
+# Congratulate and reveal to the user the number of attempts taken to narrow down to the answer. 
 if guess == secret_number:
-    print(f"Boomz!, You're right! it's {secret_number}. Not bad!, you guessed it in {num_of_attempts} attempts!")
+    print(f"Boomz!, You're right! it's {secret_number}. Not bad! you guessed it in {num_of_attempts} attempts!")
