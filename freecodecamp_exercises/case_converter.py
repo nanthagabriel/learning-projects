0d8442b9e90da4ade2ee6b3e872adfbf9c973b35
Nantha Gabriel <nanthagabriel@gmail.com>
2025-02-24 10:34:50 +0800

    Added case_converter.py to freecodecamp_exercises

---
+++ freecodecamp_exercises/case_converter.py
@@ -0,0 +1,17 @@
+# SCIENTIFIC COMPUTING WITH PYTHON
+# LEARN PYTHON LIST COMPREHENSION BY BUILDING A CASE CONVERTER PROGRAM
+
+def main():
+    print(convert_to_snake_case('IAmAPascalCasedString'))
+
+def convert_to_snake_case(pascal_or_camel_cased_string):
+
+    snake_cased_char_list = [
+        '_' + char.lower() if char.isupper()
+        else char
+        for char in pascal_or_camel_cased_string
+    ]
+
+    return ''.join(snake_cased_char_list).strip('_')
+
+main()
