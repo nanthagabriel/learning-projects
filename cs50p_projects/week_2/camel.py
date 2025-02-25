32762a437f7ebbe1c2039d9780fe66018b578cf5
Nantha Gabriel <nanthagabriel@gmail.com>
2025-02-25 15:44:27 +0800

    Added camel.py to week_2

--- cs50p_projects/week_2/camel.py
+++ cs50p_projects/week_2/camel.py
@@ -1,14 +1,24 @@
+# User input in camel case format
 def main():
     name = input("Provide name in camelCase: ")
     print(convert_snake_case(name))
 
+# Convert camel case to snake case
 def convert_snake_case(name):
     result = ""
+
+    # Loop every later from input
     for letter in name:
+
+        # Check for upper case
         if letter.isupper():
+
+            # If input is uppercase, add an underscore before it and convert it to lowercase
             result += "_"
             result += letter.lower()
         else:
+
+            # Add input to result if it's lowercase
             result += letter
     return result
 
