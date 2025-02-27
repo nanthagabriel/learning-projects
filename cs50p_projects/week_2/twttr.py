49b9062c2b957f9f10cf23fc65d2a75cd80b736d
Nantha Gabriel <nanthagabriel@gmail.com>
2025-02-27 11:49:17 +0800

    Added twttr.py to week_2

---
+++ cs50p_projects/week_2/twttr.py
@@ -0,0 +1,15 @@
+# User input
+answer = input("Input: ")
+
+# Output
+print("Output: ", end="")
+
+# Loop each letter from input
+for letter in answer:
+
+    # Check if input has vowels regardless upper or lowercase
+    if letter.lower() not in ['a', 'e', 'i', 'o', 'u']:
+        print(letter, end="")
+
+# New line
+print()
