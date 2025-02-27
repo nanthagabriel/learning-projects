261006e58bfc3a1ba0c1edc8578d155972f5c827
Nantha Gabriel <nanthagabriel@gmail.com>
2025-02-27 23:50:21 +0800

    Updated dont_give_up.py

--- personal_projects/dont_give_up.py
+++ personal_projects/dont_give_up.py
@@ -1,5 +1,6 @@
+
 # I'm currently working on this 
-required_foundations =[" CS106A", " CS50P", " Python Specialization", " Algebra"]
+required_foundations = ["CS106A", "CS50P", "Python Specialization", "Algebra"]
 
 # Check requirements and completions
 def check_foundations(completed_foundations):
@@ -7,14 +8,14 @@ def check_foundations(completed_foundations):
 
     # Only if i complete, which i will, i'll proceed to machine learning and data science  
     if missing_foundations:
-        print("\nMissing foundations:",",".join(missing_foundations))
+        print("\nMissing foundations:", ", ".join(missing_foundations))
         print("No excuses! ..Finish them before you move on to Data Science.")
     else:
         print("All done! Proceed to Data Science, Good luck Gabriel!")
 
 # Pace yourself man, one at a time
-def main(): 
-    completed_foundations = [" CS106A"] 
+def main():
+    completed_foundations = ["CS106A"] 
     check_foundations(completed_foundations)
 
 # Execute phase 2
