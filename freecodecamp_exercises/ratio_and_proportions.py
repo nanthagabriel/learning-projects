4aa4beb18ec7e9d1f5fc4c1050ec4c60d85337f2
nanthagabriel <nanthagabriel@gmail.com>
2025-03-11 14:40:45 +0800

    Added ratio_and_proportions.py to freecodecamp_exercises

---
+++ freecodecamp_exercises/ratio_and_proportions.py
@@ -0,0 +1,17 @@
+# COLLEGE ALGEBRA WITH PYTHON
+# RATIO AND PROPORTIONS
+# n1/d1 = n2/d2
+
+# Use a zero in the place of an unknown value
+n1 = 1
+d1 = 1.6
+n2 = 0
+d2 = 1
+
+if n2 == 0:
+    answer = d2 * n1 / d1
+    print("n2 = ", answer)
+
+if d2 == 0:
+    answer = n2 * d1 / n1
+    print("d2 = ", answer)
